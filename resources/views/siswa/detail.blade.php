48639960e3aa8ac9005a17e2b9540a9c8a967750
vian <galangdavianp@gmail.com>
2023-05-19 14:29:44 +0700

    100% for siswa, 0% guru n admin

--- resources/views/siswa/detail.blade.php
+++ resources/views/siswa/detail.blade.php
@@ -7,7 +7,7 @@
 
     <p class="nama-lengkap fs-1 fw-bold">{{Auth::user()->name}}</p>
 
-    <p class="kelas fs-3">{{Auth::user()->kelas}}</p>
+    <p class="kelas fs-3">{{Auth::user()->siswa->kelas->nama}}</p>
 </div>
 
 <div class="container d-flex justify-content-center align-items-center w-100 mt-4">
@@ -23,14 +23,16 @@
         <div class="col-md-6 d-flex justify-content-center align-items-center flex-column mb-3">
             <p class="nisn fw-bold mb-1">NISN</p>
             <div class="nisn-content w-100">
-                <p class="py-2 px-5 d-flex justify-content-center align-items-center">0020399393</p>
+                <p class="py-2 px-5 d-flex justify-content-center align-items-center">
+                    {{Auth::user()->siswa->nisn}}
+                </p>
             </div>
         </div>
 
     </div>
 </div>
 
-<div class="container pelanggaran w-75 border border-primary ">
+<div class="container pelanggaran w-75 border border-primary mb-5">
     <p class="title fw-bold p-3">Pelanggaran</p>
     <p class="title fw-bold px-4">Macam-Macam:</p>
 
@@ -39,39 +41,48 @@
             <tr class="fw-bold">
                 <td>No</td>
                 <td>Masalah</td>
-                <td>Point</td>
+                <td>Catatan</td>
             </tr>
+            @foreach($pelanggaran as $row)
             <tr>
-                <td>1</td>
-                <td>tess</td>
-                <td>tessss</td>
+
+                <td>{{$loop->iteration}}</td>
+                <td>{{$row->kategori->nama_kategori}}</td>
+                <td>{{$row->catatan}}</td>
             </tr>
-            <tr>
-                <td>2</td>
-                <td>tessri</td>
-            </tr>
+            @endforeach
         </table>
     </div>
 
-    <div class="container d-flex justify-content-center align-items-center w-100 mt-5">
+    <div class="container d-flex justify-content-center align-items-center w-100 mt-4">
         <div class="row w-100">
 
-            <div class="col-md-6 d-flex justify-content-center align-items-center flex-column mb-3">
+            <div class="col-md-12 d-flex justify-content-center align-items-center flex-column mb-3">
                 <p class=" fw-bold mb-1">Total Masalah</p>
                 <div class="w-100">
-                    <p class="py-2 px-5 d-flex justify-content-center align-items-center">3</p>
+                    <p class="py-2 px-5 d-flex justify-content-center align-items-center">{{$pelanggaran->count()}}</p>
                 </div>
             </div>
 
-            <div class="col-md-6 d-flex justify-content-center align-items-center flex-column mb-3">
-                <p class=" fw-bold mb-1">Total Point</p>
-                <div class="w-100">
-                    <p class="py-2 px-5 d-flex justify-content-center align-items-center">350</p>
-                </div>
-            </div>
+
 
         </div>
     </div>
 
 </div>
+
+<div class="container d-flex justify-content-center align-items-center mt-5">
+    <div class="container btn btn-primary btn-logout w-50 d-flex justify-content-center align-items-center">
+
+    <a class="text-white" href="{{ route('logout') }}"
+        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+        {{ __('Logout') }}
+    </a>
+
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+        @csrf
+    </form>
+    </div>
+</div>
+
 @endsection
