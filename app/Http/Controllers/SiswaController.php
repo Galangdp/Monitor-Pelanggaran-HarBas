77857deca250bff2fa9b9400a4dd809556d18290
vian <galangdavianp@gmail.com>
2023-05-19 14:30:26 +0700

    siswa controller commit

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Siswa;
+use App\Models\User;
+use App\Models\Kelas;
+use App\Models\Pelanggaran;
 
 class SiswaController extends Controller
 {
@@ -15,7 +18,10 @@ class SiswaController extends Controller
     //     return view('siswa.detail', compact('siswa'));
     // }
 
-    public function detail(){
-        return view('siswa.detail');
+    public function detail($id){
+        $kelas = Kelas::all();
+        $siswa = Siswa::where('id_user', $id)->get();
+        $pelanggaran = Pelanggaran::with('kategori')->where('id_siswa', $siswa-> first() ->id)->get();
+        return view('siswa.detail', compact('siswa', 'kelas', 'pelanggaran'));
     }
 }
